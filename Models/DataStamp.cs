f0cbf60c741e4b18309941a786861970bac8a8fc
Leviathan <levi.mcgomes@gmail.com>
2024-01-08 19:11:45 +0000

    Added DataStamp model.

---
+++ Models/DataStamp.cs
@@ -0,0 +1,8 @@
+﻿namespace backend24.Models
+{
+	public struct DataStamp
+	{
+		public long Timestamp { get; set; }
+		public GPSCoords Coordinates { get; set; }
+	}
+}
---
+++ Models/GPSCoords.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend24.Models
+{
+	public struct GPSCoords
+	{
+		[Range(-90,90)]
+        public float Latitude { get; set; }
+		[Range(-180, 180)]
+		public float Longitude { get; set; }
+		public float Altitude { get; set; }
+	}
+}
