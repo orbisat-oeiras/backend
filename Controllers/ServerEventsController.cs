613550694060e45a556f49f45e7f2afb4d9033af
Leviathan <levi.mcgomes@gmail.com>
2024-01-31 16:05:54 +0000

    Added a basic controller for testing server-sent events.

---
+++ Controllers/ServerEventsController.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+
+using backend24.Services.EventFinalizers;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend24.Controllers
+{
+	[ApiController]
+	[Route("api/[action]")]
+	public class ServerEventsController : ControllerBase
+	{
+		private readonly ILogger<ServerEventsController> _logger;
+		private readonly IEnumerable<EventFinalizerBase<object>> _eventFinalizers;
+
+		public ServerEventsController(ILogger<ServerEventsController> logger, IEnumerable<EventFinalizerBase<object>> eventFinalizers) { 
+			_logger = logger;
+			_eventFinalizers = eventFinalizers;
+		}
+
+		[HttpGet()]
+		public async Task SSE() {
+			Response.Headers.Add("Content-Type", "text/event-stream");
+			Response.Headers.Add("Cache-Control", "no-cache");
+			Response.Headers.Add("Connection", "keep-alive");
+
+			while(true) {
+				await Response.WriteAsync($"data: Controller at {DateTime.Now}\r\r");
+				await Response.Body.FlushAsync();
+				await Task.Delay(1000);
+			}
+		}
+	}
+}
