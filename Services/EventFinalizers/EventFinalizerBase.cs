eb5007af318b809b4d2e095eec10cf7e50b99451
Leviathan <levi.mcgomes@gmail.com>
2024-01-31 11:46:27 +0000

    Added a base class and an attribute for event finalizers.

---
+++ Services/EventFinalizers/EventFinalizerBase.cs
@@ -0,0 +1,11 @@
+﻿using backend24.Services.DataProcessors;
+using backend24.Services.DataProviders;
+
+namespace backend24.Services.EventFinalizers
+{
+	[EventFinalizer]
+	public abstract class EventFinalizerBase<T> : DataProcessorBase<T, (string, T)>
+	{
+		protected EventFinalizerBase(IDataProvider<T> provider) : base(provider) { }
+	}
+}
