7f8b25b93fc9d99de5503b0cfcc3a13f03c93e02
Leviathan <levi.mcgomes@gmail.com>
2024-01-31 11:50:41 +0000

    Added a missing comment.

--- Services/EventFinalizers/EventFinalizerBase.cs
+++ Services/EventFinalizers/EventFinalizerBase.cs
@@ -3,6 +3,11 @@ using backend24.Services.DataProviders;
 
 namespace backend24.Services.EventFinalizers
 {
+	/// <summary>
+	/// Base class for event finalizers. An event finalizer must inherit this class
+	/// and must add a tag to incoming data (along with any processing deemed necessary).
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
 	[EventFinalizer]
 	public abstract class EventFinalizerBase<T> : DataProcessorBase<T, (string, T)>
 	{
