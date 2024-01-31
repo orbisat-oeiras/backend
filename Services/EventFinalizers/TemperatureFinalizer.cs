6be69ac1ef9be170e866d78794cb6bf9b5c16bfe
Leviathan <levi.mcgomes@gmail.com>
2024-01-31 11:51:08 +0000

    Added a basic event finalizer, for testing purposes.

---
+++ Services/EventFinalizers/TemperatureFinalizer.cs
@@ -0,0 +1,20 @@
+﻿using backend24.Models;
+using backend24.Services.DataProviders;
+
+namespace backend24.Services.EventFinalizers
+{
+	/// <summary>
+	/// Finalizes a tempreature event, tagged with "primary/temperature".
+	/// </summary>
+	public class TemperatureFinalizer : EventFinalizerBase<float>
+	{
+		public TemperatureFinalizer(IDataProvider<float> provider) : base(provider) { }
+
+		protected override EventData<(string, float)> Process(EventData<float> data) {
+			return new EventData<(string, float)> {
+				DataStamp = data.DataStamp,
+				Data = ("primary/temperature", data.Data),
+			};
+		}
+	}
+}
