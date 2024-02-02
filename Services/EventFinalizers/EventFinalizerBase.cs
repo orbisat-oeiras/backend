076b16fa54564c5770b10b5075fcc9457364408e
Leviathan <levi.mcgomes@gmail.com>
2024-02-02 11:17:28 +0000

    Changed EventFinalizerBase so it always returns a (string, object). This allows for abstraction from the actual data on the controller's side.

--- Services/EventFinalizers/EventFinalizerBase.cs
+++ Services/EventFinalizers/EventFinalizerBase.cs
@@ -1,4 +1,6 @@
-﻿using backend24.Services.DataProcessors;
+﻿// Type alias for the IDataProvider specialization implemented by a finalizer
+global using IFinalizedProvider = backend24.Services.DataProviders.IDataProvider<(string tag, object content)>;
+using backend24.Services.DataProcessors;
 using backend24.Services.DataProviders;
 
 namespace backend24.Services.EventFinalizers
@@ -7,9 +9,9 @@ namespace backend24.Services.EventFinalizers
 	/// Base class for event finalizers. An event finalizer must inherit this class
 	/// and must add a tag to incoming data (along with any processing deemed necessary).
 	/// </summary>
-	/// <typeparam name="T"></typeparam>
+	/// <typeparam name="T">Type of incoming data</typeparam>
 	[EventFinalizer]
-	public abstract class EventFinalizerBase<T> : DataProcessorBase<T, (string, T)>
+	public abstract class EventFinalizerBase<T> : DataProcessorBase<T, (string, object)>
 	{
 		protected EventFinalizerBase(IDataProvider<T> provider) : base(provider) { }
 	}
