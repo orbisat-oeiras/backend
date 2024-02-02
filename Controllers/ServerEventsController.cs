67d7fd27befc3339414333a2800574b53aa36c72
Leviathan <levi.mcgomes@gmail.com>
2024-02-02 11:19:05 +0000

    Updated ServerEventsController to use the new abstracted IFinalizedProvider.

--- Controllers/ServerEventsController.cs
+++ Controllers/ServerEventsController.cs
@@ -1,21 +1,28 @@
 ﻿using System.Reflection;
 
+using backend24.Services.DataProviders;
 using backend24.Services.EventFinalizers;
 
 using Microsoft.AspNetCore.Mvc;
 
 namespace backend24.Controllers
 {
+	/// <summary>
+	/// Controller for handling server-sent events.
+	/// </summary>
 	[ApiController]
 	[Route("api/[action]")]
 	public class ServerEventsController : ControllerBase
 	{
+		// Logger provided by DI, used for printing information to all logging providers at once
 		private readonly ILogger<ServerEventsController> _logger;
-		private readonly IEnumerable<EventFinalizerBase<object>> _eventFinalizers;
+		// List of registered event finalizers, which provided ready-to-send events
+		private readonly IEnumerable<IFinalizedProvider> _eventFinalizers;
 
-		public ServerEventsController(ILogger<ServerEventsController> logger, IEnumerable<EventFinalizerBase<object>> eventFinalizers) { 
+		public ServerEventsController(ILogger<ServerEventsController> logger, IEnumerable<IFinalizedProvider> eventFinalizers) { 
 			_logger = logger;
 			_eventFinalizers = eventFinalizers;
+			_logger.LogInformation("DI provided {evtFinalizerCount} event finalizers.", _eventFinalizers.Count().ToString());
 		}
 
 		[HttpGet()]
