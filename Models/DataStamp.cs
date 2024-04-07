7d6323db612b78647aa8e7386a975f78107c3165
Leviathan <levi.mcgomes@gmail.com>
2024-04-07 14:33:20 +0100

    Removed unnecessary comment.

--- Models/DataStamp.cs
+++ Models/DataStamp.cs
@@ -9,11 +9,6 @@
 		/// Milliseconds since the Arduino was initialized, registered
 		/// when the data was sent.
 		/// </summary>
-		/// <remarks>
-		/// TODO: Maybe this should be converted to a DateTime,
-		/// which would require the addition of a processor
-		/// to convert the timestamps sent by the Arduino.
-		/// </remarks>
 		public long Timestamp { get; init; }
 		/// <summary>
 		/// Coordinates registered by the GPS when the data was sent
