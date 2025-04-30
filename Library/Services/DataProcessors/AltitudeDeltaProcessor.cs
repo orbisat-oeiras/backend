031baca6bdf15dfd1d87cd54325fa5c22d7a19dc
mslago <marcelosantoslago@gmail.com>
2025-04-30 11:01:05 +0100

    fix: update altitude delta to use barometric altitude.

--- Library/Services/DataProcessors/AltitudeDeltaProcessor.cs
+++ Library/Services/DataProcessors/AltitudeDeltaProcessor.cs
@@ -12,10 +12,19 @@ namespace backend.Library.Services.DataProcessors
         )
             : base(provider) { }
 
+        private float _last = float.NaN;
+
         protected override EventData<float> Process(EventData<float> data)
         {
-            // TODO: Check if we use GPS altitude or barometric altitude
-            return data with { Data = data.Data - data.DataStamp.Coordinates.Altitude };
+            float altitudedelta = 0;
+            if (!float.IsNaN(_last))
+                altitudedelta = data.Data - _last;
+            _last = data.Data;
+
+            return data with
+            {
+                Data = altitudedelta,
+            };
         }
     }
 }
