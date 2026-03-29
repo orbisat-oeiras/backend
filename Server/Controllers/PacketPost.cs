2a97e212c5af9181b53ef8683190ff0910d29a90
Marcelo Lago <72633651+mslago@users.noreply.github.com>
2026-03-29 11:34:59 +0100

    refactor

--- Server/Controllers/PacketPost.cs
+++ Server/Controllers/PacketPost.cs
@@ -42,42 +42,44 @@ namespace backend.Server.Controllers
                 _timeSyncService.BeginSync();
                 return Ok("Time synchronization started...");
             }
+            else
+            {
+                Packet packetWithAdjustedTimestamp = new(
+                    packet.DeviceId,
+                    (ulong)((long)packet.Timestamp + _timeSyncService.Offset),
+                    packet.Payload,
+                    packet.Type
+                );
 
-            Packet packetWithAdjustedTimestamp = new(
-                packet.DeviceId,
-                (ulong)((long)packet.Timestamp + _timeSyncService.Offset),
-                packet.Payload,
-                packet.Type
-            );
-
-            if (_serialSender == null)
-            {
-                _logger.LogError("ISerialSender service is not available.");
-                return StatusCode(500, "ISerialSender service is not available.");
+                if (_serialSender == null)
+                {
+                    _logger.LogError("ISerialSender service is not available.");
+                    return StatusCode(500, "ISerialSender service is not available.");
+                }
+                byte[] encodedData;
+                try
+                {
+                    encodedData = Encode.EncodePacket(packetWithAdjustedTimestamp);
+                }
+                catch (Exception)
+                {
+                    _logger.LogError("Timestamp size overflow, sync CanSat time.");
+                    return StatusCode(400, "Timestamp size overflow, sync CanSat time.");
+                }
+                _serialSender.SendPacket(encodedData);
+                // _logger.LogInformation(
+                //     "Encoded packet data: {encodedData}",
+                //     BitConverter.ToString(encodedData)
+                // );
+                _logger.LogInformation(
+                    "Sent packet with ID: {packetId} with CanSatTimestamp {time} with data {data}, as type {type}",
+                    packetWithAdjustedTimestamp.DeviceId,
+                    packetWithAdjustedTimestamp.Timestamp,
+                    Encoding.ASCII.GetString(packetWithAdjustedTimestamp.Payload.Value),
+                    packetWithAdjustedTimestamp.Type
+                );
+                return Ok(packetWithAdjustedTimestamp);
             }
-            byte[] encodedData;
-            try
-            {
-                encodedData = Encode.EncodePacket(packetWithAdjustedTimestamp);
-            }
-            catch (Exception)
-            {
-                _logger.LogError("Timestamp size overflow, sync CanSat time.");
-                return StatusCode(400, "Timestamp size overflow, sync CanSat time.");
-            }
-            _serialSender.SendPacket(encodedData);
-            // _logger.LogInformation(
-            //     "Encoded packet data: {encodedData}",
-            //     BitConverter.ToString(encodedData)
-            // );
-            _logger.LogInformation(
-                "Sent packet with ID: {packetId} with CanSatTimestamp {time} with data {data}, as type {type}",
-                packetWithAdjustedTimestamp.DeviceId,
-                packetWithAdjustedTimestamp.Timestamp,
-                Encoding.ASCII.GetString(packetWithAdjustedTimestamp.Payload.Value),
-                packetWithAdjustedTimestamp.Type
-            );
-            return Ok(packetWithAdjustedTimestamp);
         }
     }
 }
